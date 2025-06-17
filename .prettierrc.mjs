f21799e4c0b922ad7e048bd12a50e6ac6d6d4493
Tux <wholetthetuxout@proton.me>
2025-06-17 11:49:35 +0100

    feat: update prettier configuration
    - Add proseWrap: "always" for better text wrapping
    - Add prettier-plugin-astro for Astro file support
    - Set printWidth to 100 characters
    - Configure tabWidth to 2 spaces
    - Disable useTabs in favor of spaces
    - Enable semicolons
    - Use double quotes
    - Set trailingComma to "es5"
    - Enable bracketSpacing
    - Set arrowParens to "always"
    - Set endOfLine to "lf"
    - Add Astro file parser override

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,6 +3,15 @@
 export default {
   proseWrap: "always",
   plugins: ["prettier-plugin-astro"],
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
   overrides: [
     {
       files: "*.astro",
